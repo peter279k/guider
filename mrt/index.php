87ffd04a20016e27dd74325a78a5f2e90637d392
peter279k <peter279k@gmail.com>
2016-01-16 20:10:04 +0800

    move exit.csv and initial Slim GET method

--- mrt/index.php
+++ mrt/index.php
@@ -1,3 +1,17 @@
 <?php
-	
+	require 'vendor/autoload.php';
+
+	$app = new Slim\App();
+
+	$app->get('/get/{name}', function ($request, $response, $args) {
+		$name = htmlentities($args["name"]);
+		if($name === "exit") {
+			//parse mrt.csv file
+			
+		}
+
+		return $response;
+	});
+
+	$app->run();
 ?>
