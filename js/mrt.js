9c310f5d3b9ba519d8c89a2a0a8ed5767387bfc8
peter279k <peter279k@gmail.com>
2016-01-26 16:09:38 +0800

    adb-substep 換位置

--- js/mrt.js
+++ js/mrt.js
@@ -16,7 +16,7 @@ $(function() {
 			alertify.alert("Sorry！沒有路線可修正！");
 			return false;
 		}
-		console.log($("#panel").html());
+		console.log($("td.adp-substep").html());
 	});
 	
 	$("#submit").click(function(event) {
@@ -58,7 +58,6 @@ $(function() {
 					// direction.routes[n].legs 該路段細節
 					if (direction) {
 						//console.log(direction);
-						console.log($("td.adp-substep").html());
 						$("#directions").append("<p>距離：" + direction.routes[0].legs[0].distance.text + "</p>")
 							.append("<p>旅行時間：" + direction.routes[0].legs[0].duration.text + "</p>");
 					}
